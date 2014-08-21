299530145b689b3a6cbf2fb3cab571dff015ff9c
Marco Leite <s00149173@mail.itsligo.ie>
2014-08-21 09:58:37 +0100

    First commit

---
+++ UploadFiles/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UploadFiles.Models;
+
+namespace UploadFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateJob()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult CreateJob(NewJobViewModel job)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(job);
+            }
+
+            if (job.files.Any())
+            {
+                var files = job.files.Select(x => new { 
+                    name = x.FileName, 
+                    extension = x.ContentType, 
+                    size = x.ContentLength 
+                });
+                ViewBag.UploadedFiles = job.files;
+            }
+            return View(job);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UploadFiles/Models/NewJobViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UploadFiles.Models
+{
+    public class NewJobViewModel
+    {
+        [Required]
+        [DisplayName("Insert a Job Name")]
+        public string JobName { get; set; }
+
+        [DisplayName("Select Files")]
+        public IEnumerable<HttpPostedFileBase> files { get; set; }
+
+    }
+}
